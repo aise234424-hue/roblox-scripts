f749c5f2996e41cd7fba7a0d96c2572cbab40980
aise234424-hue <aise234424@gmail.com>
2026-01-14 22:53:04 +0500

    show coins on screen

--- ui/coins_button.lua
+++ ui/coins_button.lua
@@ -1,7 +1,11 @@
 local button = script.Parent
+local gui = button.Parent
+local label = gui:WaitForChild("CoinsLabel")
+
 local coins = 0
+label.Text = "Coins: " .. coins
 
 button.MouseButton1Click:Connect(function()
 	coins += 1
-	print(coins)
+	label.Text = "Coins: " .. coins
 end)
